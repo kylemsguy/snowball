ce439e09e640a95084a2d4a72d338502e3ea29cb
Kyle Zhou <kylezhou2002@hotmail.com>
2015-01-17 15:02:22 -0500

    did something with throw_ball

--- snowflake/throw_ball.py
+++ snowflake/throw_ball.py
@@ -3,3 +3,10 @@ from oauth2client.client import OAuth2WebServerFlow
 
 import httplib2
 
+def change_event(cal_id: str, month: int, day: int, code: str, action: int, evt_type=None) -> bool:
+	# initial 
+	# fetch data
+
+	# parse json
+	# complete action
+	return False
