cf99a59798245435ce96c7fefb48ce36fc35e19d
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-01-17 12:47:58 -0500

    Add webapp backend

---
+++ webapp/winter/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import *
+from django.views.decorators.csrf import *
+import json
+
+# Create your views here.
+
+@csrf_exempt
+def contextio_callback(request):
+	print("We got it ")
+	payload = json.loads(str(request.body, 'utf-8'))
+	print(payload)
+	return HttpResponse("Success")
+
+@csrf_exempt
+def contextio_failure_callback(request):
+	print("We failed ")
+	payload = json.loads(str(request.body, 'utf-8'))
+	print(payload)
+	return HttpResponse("Success")
