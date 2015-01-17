3c9aab694d5c86cf8f1a5d4fb5da12e48ee6882e
Kyle Zhou <kylezhou2002@hotmail.com>
2015-01-17 17:28:21 -0500

    lol throwing ball

--- frozen-lake/src/frozen-lake.c
+++ frozen-lake/src/frozen-lake.c
@@ -1,30 +1,52 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *s_time_layer;
 
-static char s_time[8] = "00:00";
+static void update_time() {
+  static char s_time[8] = "00:00";
 
-static void click_config_provider(void *context) {
+  // get a tm struct
+  time_t tmp = time(NULL);
+  struct tm *tick_time = localtime(&tmp);
 
+  if(clock_is_24h_style()) {
+    // use 24 hour format
+    strftime(s_time, sizeof(s_time), "%H:%M", tick_time);
+  } else {
+    // use 12 hour format with AM/PM
+    strftime(s_time, sizeof(s_time), "%I:%M%P", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_time);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, s_time);
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  //s_time_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  update_time();
+
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_DROID_28_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(s_time_layer);
 }
 
 static void init(void) {
   window = window_create();
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
