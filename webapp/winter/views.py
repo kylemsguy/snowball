f3ec52c8c7852db853f70dda0bb0d41230a9c720
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-01-17 21:32:58 -0500

    Save context.io auth token

--- webapp/winter/views.py
+++ webapp/winter/views.py
@@ -7,21 +7,37 @@ from django.views.decorators.cache import never_cache
 import social.apps.django_app.views
 import contextio
 import json
+from winter.models import *
+import datetime
 
 # Create your views here.
 
 @csrf_exempt
 def contextio_callback(request):
-	print("We got it ")
-	payload = json.loads(str(request.body, 'utf-8'))
-	print(payload)
-	return HttpResponse("Success")
+	try:
+		print("We got it ")
+		payload = json.loads(request.body)
+		print(payload)
+		user_profiles = UserProfile.objects.filter(context_io_account_id = payload["account_id"])
+		if len(user_profiles) == 0:
+			print("What? no profile?")
+		else:
+			user_profile = user_profiles[0]
+			user = user_profile.user
+			provider = user.social_auth.get(provider="google-oauth2")
+			handle_callback(user, user_profile, provider, payload)
+		return HttpResponse("Success")
+	except BaseException as e:
+		print e
+		raise e
+
+def handle_callback(user, user_profile, provider, payload):
+	print(user, user_profile, provider, payload)
 
 @csrf_exempt
 def contextio_failure_callback(request):
 	print("We failed ")
-	payload = json.loads(str(request.body, 'utf-8'))
-	print(payload)
+	payload = json.loads(request.body)
 	return HttpResponse("Success")
 
 def home(request):
@@ -29,7 +45,7 @@ def home(request):
 
 def link_email(request):
 	context_io = contextio.ContextIO(consumer_key = context_io_key, consumer_secret = context_io_secret)
-	connect_token = context_io.post_connect_token(callback_url = server_addr + "/link_email_callback/" + str(request.user.id),
+	connect_token = context_io.post_connect_token(callback_url = server_addr + "/link_email_callback",
 		email = request.session["school_email"], first_name = request.session["first_name"],
 		last_name = request.session["last_name"])
 	print(connect_token)
@@ -38,7 +54,15 @@ def link_email(request):
 	return HttpResponse("Failed to link email")
 
 def link_email_callback(request):
-
+	contextio_token = request.GET["contextio_token"]
+	context_io = contextio.ContextIO(consumer_key = context_io_key, consumer_secret = context_io_secret)
+	connect_token = contextio.ConnectToken(context_io, {"token": contextio_token})
+	connect_token.get()
+	new_profile = UserProfile(user=request.user, context_io_account_id=connect_token.account.id)
+	new_profile.save()
+	# attach webhook
+	connect_token.account.post_webhook(callback_url=server_addr + "/callback",
+		failure_notif_url=server_addr + "/callback_fail", sync_period="immediate", include_body=1)
 	return render(request, 'winter/success.html', {})
 
 @require_POST
