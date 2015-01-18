92a19d89283035f941f62cd5ede6efd1993fcd8c
LemonPi <johnson9510@hotmail.com>
2015-01-17 21:43:08 -0500

    More buggy parsing

--- snowflake/main.cpp
+++ snowflake/main.cpp
@@ -7,18 +7,18 @@ using namespace std;
 int main(int argc, char* argv[]) {
     switch (argc) {
         case 1:
-        	cout << "No string argument\n";
-            return 0;
+            ts.set_input(cin);
+            break;
         case 2:
             ts.set_input(new istringstream{argv[1]});
             break;
         default: 
-        	cout << "too many arguments: " << argc << '\n';
-        	return 0;
+            cout << "too many arguments: " << argc << '\n';
+            return 2;
     }
 
     auto dates = all_dates({17, 1, 2015});
     for (auto d : dates) 
-    	cout << d << endl;
+        cout << d << endl;
     if (dates.empty()) cout << "Empty dates!\n";
 }
